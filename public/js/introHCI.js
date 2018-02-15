47acf64b9a32adac71cfb62a3526523e5265193b
Miriam Wagner-Valladolid <mwagnerv@ucsd.edu>
2018-02-15 10:54:04 -0800

    ajax and facebook

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,11 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+
 }
